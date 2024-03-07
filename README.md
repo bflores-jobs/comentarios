f6495300630925e24ee68274b236d887db86b9be
bflores <bflores.jobs@gmail.com>
2024-03-07 15:52:16 -0300

    Update

--- README.md
+++ README.md
@@ -29,3 +29,15 @@ _2.- Abrir con IDE_
 
 _3.- Ejecutar en url por defecto: http://localhost:8080_
 
+
+### Base de datos H2
+
+_Base de datos creada en memoria_
+
+_Se crean 2 tablas Comentario y Region, relacionadas N:1_
+
+_Se pobla la base de datos con 5 comentarios y 16 regiones. Archivo data.sql_
+
+
+
+
---
+++ data.sql
@@ -0,0 +1,24 @@
+INSERT INTO region (id,nombre) VALUES(1,'Arica y Parinacota');
+INSERT INTO region (id,nombre) VALUES(2,'Tarapacá');
+INSERT INTO region (id,nombre) VALUES(3,'Antofagasta');
+INSERT INTO region (id,nombre) VALUES(4,'Atacama');
+INSERT INTO region (id,nombre) VALUES(5,'Coquimbo');
+INSERT INTO region (id,nombre) VALUES(6,'Valparaíso');
+INSERT INTO region (id,nombre) VALUES(7,'Metropolitana');
+INSERT INTO region (id,nombre) VALUES(8,'OHiggins');
+INSERT INTO region (id,nombre) VALUES(9,'Maule');
+INSERT INTO region (id,nombre) VALUES(10,'Ñuble');
+INSERT INTO region (id,nombre) VALUES(11,'Biobío');
+INSERT INTO region (id,nombre) VALUES(12,'La Araucanía');
+INSERT INTO region (id,nombre) VALUES(13,'Los Ríos');
+INSERT INTO region (id,nombre) VALUES(14,'Los Lagos');
+INSERT INTO region (id,nombre) VALUES(15,'Aysén');
+INSERT INTO region (id,nombre) VALUES(16,'Magallanes');
+
+INSERT INTO comentario (correo, comentario, id_region)  VALUES('rosa@gmail.com', 'Rosa de Aric...' ,1);
+INSERT INTO comentario (correo, comentario, id_region)  VALUES('jose@gmail.com', 'Marejadas en la costa' ,6);
+INSERT INTO comentario (correo, comentario, id_region)  VALUES('pedro@gmail.com', 'Nueva constitución' ,7);
+INSERT INTO comentario (correo, comentario, id_region)  VALUES('juan@gmail.com', 'El chumbeque' ,3);
+INSERT INTO comentario (correo, comentario, id_region)  VALUES('maria@gmail.com', 'Heladas en la noche' ,8);
+
+
